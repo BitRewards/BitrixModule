a3816337e5cabec7abeffe71b2c73572d77cb347
Kirill Kruglov <mr.kirill.kruglov@gmail.com>
2014-07-30 09:27:01 +0000

    options.php edited online with Bitbucket

--- lang/ru/options.php
+++ lang/ru/options.php
@@ -10,13 +10,13 @@ $MESS ['PARTNER_CODE'] = "Код партнера Giftd";
 $MESS ['PARTNER_TOKEN_PREFIX'] = "Префикс кодов подарочных карт";
 
 
-$MESS ['COMPONENT_VIEW_SETTINGS'] = "Компонент валицации купонов";
+$MESS ['COMPONENT_VIEW_SETTINGS'] = "Компонент валицации кодов подарочной карты";
 $MESS ["COMPONENT_IS_ACTIVE"] = "Использовать компонент";
 $MESS ["COMPONENT_TEMPLATE"] = "Шаблон компонента";
-$MESS ["COMPONENT_TEMPLATE_JS_COUPON_FIELD_ID"] = "Привязка к полю ввода купонов по ID";
-$MESS ["COMPONENT_TEMPLATE_JS_CALLBACK"] = "Функция-обработчик события проверки купона (javascript)";
+$MESS ["COMPONENT_TEMPLATE_JS_COUPON_FIELD_ID"] = "ID Вашего элемента для ввода кодов подарочных карт";
+$MESS ["COMPONENT_TEMPLATE_JS_CALLBACK"] = "JS функция, вызываемая после обработки кода подарочной карты";
 
-$MESS ['JS_PANEL_SETTINGS'] = "Окно Giftd";
+$MESS ['JS_PANEL_SETTINGS'] = "Графический блок Giftd";
 $MESS ["JS_PANEL_IS_ACTIVE"] = "Отображать вкладку Giftd";
 $MESS ["JS_TAB_POSITION"] = "Местоположение вкладки Giftd";
 $MESS ["JS_TAB_PANEL_BG_COLOR"] = "Свойство вкладки tab.panelBg.color";
