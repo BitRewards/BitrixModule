7fe379f33d5407749581de1290f18f0123083e9a
Kirill Kruglov <mr.kirill.kruglov@gmail.com>
2014-08-01 13:38:34 +0000

    add descriptions for component settings

--- lang/ru/options.php
+++ lang/ru/options.php
@@ -15,9 +15,11 @@ $MESS ["COMPONENT_TEMPLATE"] = "Шаблон компонента";
 $MESS ["COMPONENT_TEMPLATE_JS_COUPON_FIELD_ID"] = "ID Вашего элемента для ввода кодов подарочных карт";
 $MESS ["COMPONENT_TEMPLATE_JS_CALLBACK"] = "JS функция, вызываемая после обработки кода подарочной карты";
 
-$MESS ["COMPONENT_TEMPLATE_HELP"] = "Бла бла бла ";
-$MESS ["COMPONENT_TEMPLATE_JS_COUPON_FIELD_ID_HELP"] = "2 Бла бла бла ";
-$MESS ["COMPONENT_TEMPLATE_JS_CALLBACK_HELP"] = " 1Бла бла бла ";
+$MESS ["COMPONENT_TEMPLATE_HELP"] = '<p style="word-break: break-all;">1. Используя шаблон "<b>Серверный обработчик</b>" вы можете проверять коды Giftd, самостоятельно отправляя POST-запросы в формате <b>JSON {coupon: "код купона"}</b> серверному обработчику /bitrix/components/giftd/coupon_input/ajax.php</p>';
+$MESS ["COMPONENT_TEMPLATE_HELP"] .= '<p style="word-break: break-all;">2. При использовании компонента "<b>giftd:coupon_input</b>" с шаблоном "<b>Серверный обработчик и ajax-валидатор</b>", на странице, где был размещен компонент, будет автоматически встроен скрипт проверки кодов Giftd </p>';
+$MESS ["COMPONENT_TEMPLATE_HELP"] .= '<p style="word-break: break-all;">3. При использовании компонента "<b>giftd:coupon_input</b>" с шаблоном "<b>Серверный обработчик, ajax и html-форма</b>", на странице, где был размещен компонент, будет автоматически встроены скрипт проверки кодов Giftd и поле для ввода кодов &lt;input type="text" name="coupon"&gt;</p>';
+$MESS ["COMPONENT_TEMPLATE_JS_COUPON_FIELD_ID_HELP"] = "Укажите параметр id вашего поля для ввода скидочных кодов, при изменении которого скрипт компонента <b>giftd:coupon_input</b> отправит запрос на проверку кода";
+$MESS ["COMPONENT_TEMPLATE_JS_CALLBACK_HELP"] = "Укажите имя вашей js-функии, которой будет передан результат проверки кода Giftd, после того как скрипт компонета <b>giftd:coupon_input</b> проверит введенный код";
 
 $MESS ["JS_PANEL_IS_ACTIVE"] = "Отображать на сайте графический блок Giftd";
 $MESS ["JS_TAB_POSITION"] = "Местоположение вкладки Giftd";
