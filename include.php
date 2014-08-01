bb301bebdeae0b6a4c26b2db33ef2dfc7a9e7554
Kirill Kruglov <mr.kirill.kruglov@gmail.com>
2014-08-01 12:59:11 +0000

    refactor generation of settings' html

--- include.php
+++ include.php
@@ -4,10 +4,14 @@ $module_id = 'giftd.coupon';
 CModule::AddAutoloadClasses(
     $module_id,
     array(
+        "IHtmlBuilder" => "general/IHtmlBuilder.php",
         "GCouponAdmin" => "general/admin.php",
         "GiftdHelper" => "general/giftd_helper.php",
         "GiftdClient" => "general/giftd_api_client.php",
         "GiftdDiscountManager" => "general/giftd_discount_manager.php",
+        "GiftdPanelSettings" => "general/giftd_panel_settings.php",
+        "GiftdComponentSettings" => "general/giftd_component_settings.php",
+        "GenericHtmlBuilder" => "general/html_builder.php",
     )
 );
 
