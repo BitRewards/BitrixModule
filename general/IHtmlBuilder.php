7c2f6eeab3fc2b2a659b3143793ab40918803fa1
Kirill Kruglov <mr.kirill.kruglov@gmail.com>
2014-08-01 12:58:08 +0000

    refactor generation of settings' html

---
+++ general/IHtmlBuilder.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kjah
+ * Date: 01.08.14
+ * Time: 8:53
+ */
+interface IHtmlBuilder
+{
+    public function GenericInputField($name, $value);
+
+    public function OneColumnTableRow($html, $properties = '');
+
+    public function GenericTableRow($keyHtml, $valHtml, $properties = '');
+
+    public function GenericInputFileField($name, $value);
+
+    public function GenericInputCheckboxField($name, $value, $title, $isChecked);
+
+    public function GenericInputRadioField($name, $values, $checkedValue);
+
+    public function GenericTableColumn($innerHtml, $properties);
+
+    public function GenericSelectField($name, $values, $selectedValue);
+
+}
